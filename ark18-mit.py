194a51d52f1875d6232c91e0c5753d76520fcefb
QZLin <qzlin01@163.com>
2021-07-08 22:05:24 +0800

    adjust code

--- ark18-mit.py
+++ ark18-mit.py
@@ -1,6 +1,9 @@
 import logging
+from random import choice, randint
 
 import mitmproxy.http
+
+DEBUG = True
 
 logger = logging.getLogger('ark18')
 logger.setLevel(level=logging.INFO)
@@ -13,24 +16,23 @@ sh = logging.StreamHandler()
 sh.setLevel(logging.INFO)
 sh.setFormatter(formatter)
 
-logger.addHandler(fh)
+if DEBUG:
+    logger.addHandler(fh)
 logger.addHandler(sh)
 
 logger.info('ark18 start v1.18/0')
 
 
-def strs(*values, midchar=' '):
-    vs = [str(x) + midchar for x in values]
-    return ''.join(vs)[:-1]
+def strs(*values, mid_char=' '):
+    vs = [str(x) for x in values]
+    return mid_char.join(vs)
 
 
 class Ark18:
+    intervals = [1832, 2324, 3726, 4649, 4117, 4648, 5027]
+    content = '{"result":0,"message":"OK","interval":4649,"timeLeft":-1,"alertTime":600}'
 
-    # def request(self, flow: http.HTTPFlow) -> None:
-    #     pass
-
-    @staticmethod
-    def response(flow: mitmproxy.http.HTTPFlow):
+    def response(self, flow: mitmproxy.http.HTTPFlow):
         if 'hypergryph' in flow.request.pretty_host:
             logger.info(strs(flow.request.pretty_host, flow.request.pretty_url))
         else:
@@ -38,9 +40,11 @@ class Ark18:
         if flow.request.pretty_host == 'as.hypergryph.com' and \
                 flow.request.pretty_url == 'https://as.hypergryph.com/online/v1/ping':
             logger.info('***Edit Minor Status***')
-            logger.info(strs('->content', flow.response.data.content.decode()))
-            flow.response.set_text('{"result":0,"message":"OK","interval":4649,"timeLeft":-1,"alertTime":600}')
-            logger.info(strs('content->', flow.response.data.content.decode()))
+
+            logger.info(strs('->content', flow.response.data.content.decode(), '-' * 9))
+            flow.response.text = '{"result":0,"message":"OK","interval":%d,"timeLeft":-1,"alertTime":600}' % \
+                                 randint(1500, 4700)
+            logger.info(strs('-' * 9, flow.response.data.content.decode(), 'content->'))
             return
 
 
